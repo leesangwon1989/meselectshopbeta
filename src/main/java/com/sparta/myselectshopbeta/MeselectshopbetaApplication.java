ee3a00560981cf7e77aedd514feb1b5e789b4269
snagwon <ezlsw@naver.com>
2023-02-11 09:40:42 +0900

    Initial commit

---
+++ src/main/java/com/sparta/myselectshopbeta/MeselectshopbetaApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.myselectshopbeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeselectshopbetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeselectshopbetaApplication.class, args);
+    }
+
+}
